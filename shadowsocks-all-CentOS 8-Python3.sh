b56fec846df394c41e9cc889d618335413997045
Gabriel <yanzeyu2011nian@163.com>
2022-02-11 17:31:39 +0800

    [fix] fix bug

--- shadowsocks-all-CentOS 8-Python3.sh
+++ shadowsocks-all-CentOS 8-Python3.sh
@@ -1222,10 +1222,10 @@ install_cleanup(){
 }
 
 config_bbr(){
-	if ${bbr} 1; then 
+	if [ "${bbr}" == '1' ]; then 
 	echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 	echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-	ysctl -p
+	sysctl -p
 	
 	echo
 	echo "BBR configuration finished, please reboot the server"
